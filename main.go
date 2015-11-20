13855e7410c208fc7d8ce00fb04014052baa1614
Adrian Zankich <azankich@pivotal.io>
2015-11-19 17:35:41 -0800

    Display version number with -v flag
    Signed-off-by: Adrian Zankich <azankich@pivotal.io>

--- main.go
+++ main.go
@@ -4,10 +4,20 @@ import (
 	"fmt"
 	"github.com/zankich/concourse-scratch/client1"
 	"github.com/zankich/concourse-scratch/client2"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
-	fmt.Println(ReturnThree())
+	if len(os.Args) == 2 && (os.Args[1] == "-v" || os.Args[1] == "--version") {
+		if buf, err := ioutil.ReadFile("version"); err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(string(buf))
+		}
+		os.Exit(0)
+	}
+	fmt.Printf("The answer is: %d!\n", ReturnThree())
 }
 
 func ReturnThree() int {
